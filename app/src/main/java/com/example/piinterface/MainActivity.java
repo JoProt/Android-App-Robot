1a4bc9d86f211a18b44ae26d0eeec71aa2cbcdd7
JoProt <47474907+JoProt@users.noreply.github.com>
2020-04-01 09:09:52 +0200

    added socket programming

--- app/src/main/java/com/example/piinterface/MainActivity.java
+++ app/src/main/java/com/example/piinterface/MainActivity.java
@@ -1,86 +1,83 @@
 package com.example.piinterface;
 
-import android.graphics.Color;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.*;
+
+
 public class MainActivity extends AppCompatActivity {
 
-    Button up, left, right, down, close, open, circLeft, circRight;
-    ImageView connecStat;
-
     private static final String TAG = "Button Clicked";
-
+    private static Socket socket;
+    private static PrintWriter printWriter;
+    int msgCode = 0;
+    private static String ip = "192.168.178.40";
+    int port = 5000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //refrenz Variables
-        up = findViewById(R.id.buttonUP);
-        left = findViewById(R.id.buttonLEFT);
-        right =findViewById(R.id.buttonRIGHT);
-        down =findViewById(R.id.buttonDOWN);
-        close = findViewById(R.id.buttonCLOSE);
-        open = findViewById(R.id.buttonOPEN);
-        circLeft = findViewById(R.id.buttonCIRCLEFT);
-        circRight = findViewById(R.id.buttonCIRCRIGHT);
-        connecStat = findViewById(R.id.CONNSTST);
-        //--------------------------------------------
 
+    }
 
-        up.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.v(TAG,"Up");
-                connecStat.setColorFilter(Color.GREEN);
-            }
-        });
-        right.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.v(TAG,"Right");
-            }
-        });
-        left.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.v(TAG,"Left");
-            }
-        });
-        down.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.v(TAG,"Down");
-                connecStat.setColorFilter(Color.RED);
-            }
-        });
-        close.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.v(TAG,"Close");
-            }
-        });
-        open.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.v(TAG,"Open");
-            }
-        });
-        circRight.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.v(TAG,"Circle Right");
-            }
-        });
-        circLeft.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.v(TAG,"Circle Left");
-            }
-        });
+    public void OnClickUp(View v){
+        SendToSocket(1);
+    }
+    public void OnClickRight(View v){
+        SendToSocket(2);
+    }
+    public void OnClickLeft(View v){
+        SendToSocket(3);
+    }
+    public void OnClickDown(View v){
+        SendToSocket(4);
+    }
+    public void OnClickOpen(View v){
+        SendToSocket(5);
+    }
+    public void OnClickClose(View v){
+        SendToSocket(6);
+    }
+    public void OnClickCirlceLeft(View v){
+        SendToSocket(7);
+    }
+    public void OnClickCirlceRight(View v){
+        SendToSocket(8);
+    }
 
+    private void SendToSocket(int code){
+        this.msgCode = code;
+
+        MSGTask msgTask = new MSGTask();
+        msgTask.execute();
+
+        Toast.makeText(getApplicationContext(),"gesendet:"+code, Toast.LENGTH_SHORT).show();
+    }
+
+    class MSGTask extends AsyncTask<Void,Void,Void>{
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                socket = new Socket(ip, port);
+                printWriter = new PrintWriter(socket.getOutputStream());
+                printWriter.write(msgCode);
+                printWriter.flush();
+                printWriter.close();
+
+                socket.close();
+
+            }
+            catch (IOException io){
+                io.printStackTrace();
+            }
+            return null;
+        }
     }
 }
